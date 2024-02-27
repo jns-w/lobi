d104aed963add5ce2b2ea315127a50ff78f2fdb0
jns-w <jns.w@icloud.com>
2024-02-27 20:18:35 +0800

    production api endpoint fix

--- lobi-cli/next.config.js
+++ lobi-cli/next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {}
 
 module.exports = {
     ...nextConfig,
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8080/api/:path*'
-            }
-        ]
-    }
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: 'http://localhost:8080/api/:path*'
+    //         }
+    //     ]
+    // }
 }
