08737cb82fa18ccab17a3afcdcb1783c8f67a272
jns-w <jns.w@icloud.com>
2024-02-29 13:08:51 +0800

    next env test

--- lobi-cli/next.config.js
+++ lobi-cli/next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {}
 module.exports = {
     ...nextConfig,
     env: {
-        API_URL: process.env.NEXT_PUBLIC_API_ENDPOINT
+        API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                // destination: 'http://localhost:8080/api/:path*'
-                destination: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/:path*`
-            }
-        ]
-    }
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: 'http://localhost:8080/api/:path*'
+    //             // destination: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/:path*`
+    //         }
+    //     ]
+    // }
 }
